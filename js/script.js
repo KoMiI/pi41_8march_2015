57f4117a8a0169622da88d2f3e2471444eda45ca
Inspector97 <artem.fast@gmail.com>
2015-03-05 23:42:59 +0600

    Review of "что то"

--- js/script.js
+++ js/script.js
@@ -119,7 +119,30 @@ function textt(input)
 	return input.replace(new RegExp("@NAME@",'g'),name);
 }
 
-
+function get_css()
+{
+	var datetoday = new Date();
+	var timenow = datetoday.getTime();
+	datetoday.setTime(timenow);
+	var thehour = datetoday.getHours();
+	var dis; 	
+	if (thehour > 20)
+		dis = "css/night.css";
+	else if (thehour > 17)
+		dis = "css/sunset.css";
+	else if (thehour > 12)
+		dis = "css/afternoon.css";
+	else if (thehour > 7)
+		dis = "css/morning.css";
+	else if (thehour > 4)
+		dis = "css/sunrise.css";
+	else 
+		dis = "css/night.css";
+	var css = "<"; 
+	css+="link rel='stylesheet' href='" + dis + "' \/"; 
+	css+=">";
+	document.write(css);
+}
 
 
 
