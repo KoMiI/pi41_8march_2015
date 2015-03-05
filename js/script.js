ef77c6326b5c98a621722b9330d9094fbd1b9e40
idib <id.ib.mail@gmail.com>
2015-03-05 23:20:07 +0600

    что то
    bug in script.js on 94 line

--- js/script.js
+++ js/script.js
@@ -8,11 +8,12 @@ var select_on_off = false;
 // 		global
 
 function select_name(){
+	var nameZ = document.getElementById('name_class').style.marginTop = window.innerHeight/4*1.5 +'px';
 	var name = document.getElementById('name_class').getElementsByClassName('selected');
 	for(i = 0; i < name.length; i++)
 	{
 		name[i].style.zIndex = 999-i;
-		name[i].style.top = i*5 + "px";
+		name[i].style.top = i*5 + 5 + "px";
 		name[i].style.left = i + 1 + "px";
 	}
 	
@@ -26,6 +27,7 @@ function select_u_d(){
 		console.log("2");
 		for(i = 0; i < name.length; i++)
 		{
+			name[i].className = "selected";
 			name[i].style.top = i*5 + "px";
 			name[i].style.left = i + 1 + "px";
 		}
@@ -33,6 +35,7 @@ function select_u_d(){
 		console.log("3");
 		for(i = 0; i < name.length; i++)
 		{
+			name[i].className = "selected active";
 			name[i].style.top = (i + 1)*80 + "px";
 			name[i].style.left = "0px";
 		}
@@ -55,18 +58,18 @@ function click_name(e)
 			switch (Math.floor(Math.random() * (4))){
 				case 0:
 					x = Math.floor(Math.random() * (window.innerWidth + 1));
-					y = -Math.floor(window.innerHeight/2 + 200);
+					y = -Math.floor(window.innerHeight + 200);
 				break;
 				case 1:
-					x = Math.floor(window.innerWidth/2 + 200);
+					x = Math.floor(window.innerWidth + 200);
 					y = Math.floor(Math.random() * (window.innerHeight + 1));
 				break;
 				case 2:
 					x = Math.floor(Math.random() * (window.innerWidth + 1));
-					y = Math.floor(window.innerHeight/2 + 200);
+					y = Math.floor(window.innerHeight + 200);
 				break;
 				case 3:
-					x = -Math.floor(window.innerWidth/2 + 200);
+					x = -Math.floor(window.innerWidth + 200);
 					y = Math.floor(Math.random() * (window.innerHeight + 1));
 				break;
 			}
@@ -74,7 +77,79 @@ function click_name(e)
 			name[i].style.left = x + "px";
 		}
 	}
+	generet();
+	xmlParser();
 }
+
+function generet()
+{
+	$.ajax({
+        type: "GET",
+        url: "xml/text.xml",
+        dataType: "xml",
+        success: xmlParser
+    });
+}
+
+function xmlParser(xml) {
+	var next;
+	console.log($(xml).find("start").text());
+	$(xml).find("start").each(function () {
+		console.log($(this).find("ligament").text());
+		next = next_text($(this).find("ligament").text());
+	});
+	while(next != "Null"){
+		console.log($(xml).find(next).find("text").text());
+		$("#text_class").append(textt($(xml).find(next).find("text").text()));
+		next = next_text($(xml).find(next).find("ligament").text());
+		console.log(next);
+		$("next").fadeIn(1000);
+	}
+	
+}
+
+function next_text(input){
+	console.log(input);
+	arr = input.split("\n ");
+	return arr[Math.floor(Math.random()*arr.length)];
+}
+
+function textt(input)
+{
+	return input.replace(new RegExp("@NAME@",'g'),name);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
